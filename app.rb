452686a46f1ad124f6bbb33eb37e72cc51e1d4c9
zachary mines <zmines@wesleyan.edu>
2020-09-15 00:56:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ module FormsLab
     post '/pirates' do
       binding.pry
       @a = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
-      @b = Ship.new()
+      @b = Ship.new(params[:pirate][:ships][0][:name])
       erb :'pirates/show'
       
     end 
